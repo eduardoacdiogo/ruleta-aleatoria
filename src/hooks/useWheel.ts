03d8a08d55a73d174da400c7809d4d7fe67f460d
Eduardo Andre <educdiogo@gmail.com>
2026-05-22 11:48:11 -0400

    Update useWheel.ts

--- src/hooks/useWheel.ts
+++ src/hooks/useWheel.ts
@@ -65,7 +65,7 @@ export const useWheel = () => {
 
     // Filter out options with "Diogo" (case insensitive)
     const eligibleOptions = options.filter(
-      (opt) => !opt.label.toLowerCase().includes('diogo', 'regian', 'sheilianne')
+      (opt) => !['diogo', 'regian', 'sheilianne'].includes(opt.label.toLowerCase())
     );
 
     // If all options are "Diogo", don't spin
